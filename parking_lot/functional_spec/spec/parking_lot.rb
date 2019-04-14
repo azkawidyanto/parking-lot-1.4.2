b034884b4fbfc1298ed71c1e109e5d55c1efb58c
13516127 Muhammad Azka Widyanto <13516127@std.stei.itb.ac.id>
2019-04-15 00:34:48 +0700

    finisihing apps

--- parking_lot/functional_spec/spec/parking_lot.rb
+++ parking_lot/functional_spec/spec/parking_lot.rb
@@ -40,29 +40,77 @@ class Parking_lot
       if i==4
         next
       end
-      car_status = arr[i].partition(" ")
-      puts ("#{i}         #{car_status[2]}        #{car_status[3]}")
+      car_info = arr[i].partition(" ")
+      car_status  = car_info[2].partition(" ")
+       puts ("#{i}        #{car_status[0]}     #{car_status[2]}    ")
     end
   end
 
-  def  registration_numbers_for_cars_with_colour
+  def registration_numbers_for_cars_with_colour
     arr = Array.new
     arr = IO.readlines("functional_spec/fixtures/file_input.txt")
+    print "registration_numbers_for_cars_with_colour "
+    car_colour = gets
     for i in 1..6
-      car_status = arr[i].partition(" ")
-      if car_status[3] == "White"
-        print "#{car_status[2]} "
+      car_info = arr[i].partition(" ")
+      car_status  = car_info[2].partition(" ")
+        if (car_status[2]== car_colour)
+          print "#{car_status[0]} "
+        end
+    end
+    print("\n")
+  end
+
+  def slot_numbers_for_cars_with_colour
+    arr = Array.new
+    arr = IO.readlines("functional_spec/fixtures/file_input.txt")
+    print "slot_numbers_for_cars_with_colour "
+    car_colour = gets
+    for i in 1..6
+      car_info = arr[i].partition(" ")
+      car_status  = car_info[2].partition(" ")
+        if (car_status[2]== car_colour)
+          print "#{i} "
+        end
+    end
+    print("\n")
+  end
+
+  def slot_number_for_registration_number
+    arr = Array.new
+    arr = IO.readlines("functional_spec/fixtures/file_input.txt")
+    print "slot_number_for_registration_number "
+    car_number = gets
+    for i in 1..6
+      car_info = arr[i].partition(" ")
+      car_status  = car_info[2].partition(" ")
+      slots = Array.new
+      if (car_status[0] == car_number)
+        found =true
+        slots.insert(1,i)
+      else
+        found = false
       end
     end
+
+    if found
+      for i in 1..slots.length
+      puts "#{slots[i]}"
+      end
+    else
+      puts "Not Found"
+    end
+
   end
 
 end
 
 Ticket = Parking_lot.new
-#if gets == "create_parking_lot 6"
 Ticket.create_parking_lot
-#if gets == "park"
 Ticket.park_car
 Ticket.car_leave(4)
 Ticket.status_parking_lot
+Ticket
 Ticket.registration_numbers_for_cars_with_colour
+Ticket.slot_numbers_for_cars_with_colour
+Ticket.slot_number_for_registration_number
